7a86d261ab0765b6dae5f88b7697f30ce7af53c9
Aleksandr Podivilov <api.dark@gmail.com>
2025-10-27 00:23:19 +0300

    Test connection with properties

--- src/core/chemistry_operations.py
+++ src/core/chemistry_operations.py
@@ -30,20 +30,47 @@ class ChemistryOperations:
         """
         return self.reducer.reduce(mol, connection_atom_idx)
 
-    def connect(self, base_mol, base_mol_connection_atom_idx, additional_mol, additional_mol_connection_atom_idx):
+    def connect(self, base_mol, base_mol_connection_atom_idx, additional_mol, additional_mol_connection_atom_idx,
+                generation=None, branch=None, step=None):
         """
         Соединяет две молекулы через указанные атомы соединения.
         Возвращает новую молекулу с сохраненными метаданными.
+
+        Args:
+            base_mol: базовая молекула
+            base_mol_connection_atom_idx: индекс атома соединения в базовой молекуле
+            additional_mol: добавляемая молекула
+            additional_mol_connection_atom_idx: индекс атома соединения в добавляемой молекуле
+            generation: значение для свойства generation атомов добавляемой молекулы
+            branch: значение для свойства branch атомов добавляемой молекулы
+            step: значение для свойства step атомов добавляемой молекулы
         """
         # Уменьшаем обе молекулы согласно их replacement group спецификациям
         reduced_base = self.reducer.reduce(base_mol, base_mol_connection_atom_idx)
         reduced_additional = self.reducer.reduce(additional_mol, additional_mol_connection_atom_idx)
+
+        # Устанавливаем свойства для атомов добавляемой молекулы, если параметры заданы
+        if any(param is not None for param in [generation, branch, step]):
+            self.set_atom_properties(reduced_additional, generation, branch, step)
 
         # Соединяем уменьшенные молекулы
         connected_mol = self._connect_reduced(reduced_base, base_mol_connection_atom_idx,
                                               reduced_additional, additional_mol_connection_atom_idx)
 
         return connected_mol
+
+    def set_atom_properties(self, mol, generation, branch, step):
+        """
+        Устанавливает свойства для всех атомов молекулы.
+        """
+        for atom in mol.GetAtoms():
+            if generation is not None:
+                atom.SetProp("generation", str(generation))
+            if branch is not None:
+                atom.SetProp("branch", str(branch))
+            if step is not None:
+                atom.SetProp("step", str(step))
+
 
     def _connect_reduced(self, base_mol, base_conn_idx, additional_mol, additional_conn_idx):
         """
